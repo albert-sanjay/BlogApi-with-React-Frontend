54b222417b359b1669d20b0d0917749950982e71
Albert Sanjay <kobbydanz@hotmail.com>
2021-10-27 16:44:36 +0000

    API Documentation

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from os import name
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -30,5 +32,11 @@ urlpatterns = [
     path("api/", include('api.urls', namespace='api')),
     path("api/user/", include('users.urls', namespace='users')),
     path("api-auth/", include('rest_framework.urls', namespace='rest_framework')),
+    path("docs/", include_docs_urls(title='BlogApi')),
+    path("schema/", get_schema_view(
+        title='BlogAPI',
+        description='API for the BlogAPI',
+        version='1.0.0'
+    ), name='openapi-schema'),
     
 ]
